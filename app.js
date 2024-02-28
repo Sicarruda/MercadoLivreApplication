ecd225053ff65246531f6acd7e49792ff8637a92
Jessica Arruda <sicarruda@gmail.com>
2024-02-28 16:32:00 -0300

    first commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, './public/pictures'),
-  filename: (req, file, cb) => cb(null, Date.now() + file.originalname)
+  filename: (req, file, cb) => cb(null, Date.now() + file.originalname),
 });
 
 const upload = multer({ storage });
@@ -21,14 +21,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(helmet());
-app.use(session({
-  name: 'session',
-  keys: ['bd7126f457237e4aab0d47124ce4aac2', '9009def68579d15d871a5bf346422839'],
-  cookie: {
-    httpOnly: true,
-    expires: new Date(Date.now() + 60 * 60 * 1000 * 2) // 2 horas
-  },
-}));
+app.use(
+  session({
+    name: 'session',
+    keys: [
+      'bd7126f457237e4aab0d47124ce4aac2',
+      '9009def68579d15d871a5bf346422839',
+    ],
+    cookie: {
+      httpOnly: true,
+      expires: new Date(Date.now() + 60 * 60 * 1000 * 2), // 2 horas
+    },
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -50,14 +55,18 @@ app.get('/home', validateToken, async (req, res) => {
   try {
     const meliObject = new MeliObject(res.locals.access_token);
     const user = await meliObject.get('/users/me');
-    const categories = await meliObject.get(`/sites/${user.site_id}/categories`);
+    const categories = await meliObject.get(
+      `/sites/${user.site_id}/categories`
+    );
     const currencies = await meliObject.get('/currencies');
-    const listing_types = await meliObject.get(`/sites/${user.site_id}/listing_types`);
+    const listing_types = await meliObject.get(
+      `/sites/${user.site_id}/listing_types`
+    );
     res.render('home', {
       user,
       categories,
       currencies,
-      listing_types
+      listing_types,
     });
   } catch (err) {
     console.log('Something went wrong', err);
@@ -69,7 +78,11 @@ app.post('/post', validateToken, upload.single('picture'), async (req, res) => {
   try {
     const meliObject = new MeliObject(res.locals.access_token);
     const user = await meliObject.get('/users/me');
-    const predict = await meliObject.get(`/sites/${user.site_id}/category_predictor/predict?title=${encodeURIComponent(req.body.title)}`);
+    const predict = await meliObject.get(
+      `/sites/${
+        user.site_id
+      }/category_predictor/predict?title=${encodeURIComponent(req.body.title)}`
+    );
     await meliObject.post('/items', {
       title: req.body.title,
       category_id: predict.id,
@@ -80,17 +93,23 @@ app.post('/post', validateToken, upload.single('picture'), async (req, res) => {
       listing_type_id: req.body.listing_type,
       condition: req.body.condition,
       description: req.body.description,
-      tags: [ 'immediate_payment' ],
+      tags: ['immediate_payment'],
       pictures: [
         {
-          source: `${req.protocol}://${req.get('host')}/pictures/${req.file.filename}`
-        }
-      ]
+          source: `${req.protocol}://${req.get('host')}/pictures/${
+            req.file.filename
+          }`,
+        },
+      ],
     });
     console.log('publicado na categoria:', predict.name);
-    console.log('category probability (0-1):', predict.prediction_probability, predict.variations);
+    console.log(
+      'category probability (0-1):',
+      predict.prediction_probability,
+      predict.variations
+    );
     res.redirect('/posts');
-  } catch(err) {
+  } catch (err) {
     console.log('Something went wrong', err);
     res.status(500).send(`Error! ${err}`);
   }
